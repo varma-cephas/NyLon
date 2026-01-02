40ab608eed80ec72d80690991183a1ea8a2ca643
Varma Cephas <varmac231@gmail.com>
2026-01-02 16:59:57 +0200

    feat: set up enviroment for using .env files, add modules, and setup function for presign urls

--- apps/api/src/presign-url/presign-url.service.ts
+++ apps/api/src/presign-url/presign-url.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class PresignUrlService {}
+export class PresignUrlService {
+    constructor(private configService: ConfigService) {}
+    private presignUrl(file: any){
+        console.info(this.configService.getOrThrow('DATABASE_USER'))
+        return file
+    }
+    generateFilesPresignedUrl(files: any){
+        return this.presignUrl(files)
+    }
+}
